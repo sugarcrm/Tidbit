c816bad2391b944a2e81515fa3728d4a792413ba
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2018-03-15 17:49:20 +0300

    Refactor module data generation process

---
+++ src/Generator/Controller.php
@@ -0,0 +1,138 @@
+<?php
+/*********************************************************************************
+ * Tidbit is a data generation tool for the SugarCRM application developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2010 SugarCRM Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ ********************************************************************************/
+
+namespace Sugarcrm\Tidbit\Generator;
+
+use Sugarcrm\Tidbit\InsertBuffer;
+
+class Controller
+{
+    /**
+     * Generatro
+     *
+     * @var Generator
+     */
+    protected $g;
+
+    /**
+     * SugarBean
+     *
+     * @var \SugarBean
+     */
+    protected $bean;
+
+    /**
+     * Activity Generator
+     *
+     * @var Activity
+     */
+    protected $activityGenerator;
+
+    public function __construct(Generator $g, \SugarBean $bean, Activity $activity)
+    {
+        $this->g = $g;
+        $this->bean = $bean;
+        $this->activityGenerator = $activity;
+    }
+
+    public function generate($total)
+    {
+        $buffers = [];
+        $generatedIds = [];
+        showProgress(0, $total);
+        for ($i = 0; $i < $total; $i++) {
+            $data = $this->g->generateRecord($i);
+            $generatedIds[] = $data['id'];
+            foreach ($data['data'] as $table => $rows) {
+                if (!isset($buffers[$table])) {
+                    $buffers[$table] = new InsertBuffer($table, $GLOBALS['storageAdapter']);
+                }
+
+                foreach ($rows as $row) {
+                    $buffers[$table]->addInstallData($row);
+                }
+            }
+
+            if ($i % (int)(max(1, min($total/100, 1000))) == 0) {
+                showProgress($i, $total);
+            }
+        }
+
+        foreach ($buffers as $buffer) {
+            $buffer->flush();
+        }
+
+        showProgress($total, $total);
+
+        if ($this->bean->getModuleName() == 'Teams') {
+            $teamGenerator = new TeamSets(
+                $GLOBALS['db'],
+                $GLOBALS['storageAdapter'],
+                $generatedIds,
+                $GLOBALS['maxTeamsPerSet']
+            );
+            $teamGenerator->generate();
+        }
+
+        if ($this->bean->getModuleName() == 'Users') {
+            $prefGenerator = new UserPreferences($GLOBALS['db'], $GLOBALS['storageAdapter']);
+            $prefGenerator->generate($generatedIds);
+            if (!empty($GLOBALS['as_populate'])) {
+                $this->activityGenerator->setUserIds($generatedIds);
+            }
+        }
+
+        // Apply TBA Rules for some modules
+        // $roleActions are defined in configs
+        if ($this->bean->getModuleName() == 'ACLRoles') {
+            $tbaGenerator = new \Sugarcrm\Tidbit\Generator\TBA($GLOBALS['db'], $GLOBALS['storageAdapter']);
+
+            if (isset($GLOBALS['clean'])) {
+                $tbaGenerator->clearDB();
+            } elseif (isset($GLOBALS['obliterate'])) {
+                $tbaGenerator->obliterateDB();
+            }
+
+            if (!empty($GLOBALS['tba'])) {
+                $tbaGenerator->setAclRoleIds($generatedIds);
+                $tbaGenerator->setRoleActions($GLOBALS['roleActions']);
+                $tbaGenerator->setTbaFieldAccess($GLOBALS['tbaFieldAccess']);
+                $tbaGenerator->setTbaRestrictionLevel($GLOBALS['tbaRestrictionLevel']);
+                $tbaGenerator->generate();
+            }
+        }
+    }
+}
