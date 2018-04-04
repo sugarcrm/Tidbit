81fb0e858c6b600e89054c537112f80b87b166b6
Alexey Klimko <aklimko@sugarcrm.com>
2018-04-04 16:19:35 +0200

    Baseline profine based on the data from real customers.

---
+++ config/profiles/baseline.php
@@ -0,0 +1,46 @@
+<?php
+
+$modules = array(
+    'Tags' => 400,
+    'EmailAddresses' => 2300000,
+    'ACLRoles' => 30,
+    'Users' => 2400,
+    'Teams' => 3000,
+    'Accounts' => 300000,
+    'Quotes' => 10,
+    'ProductBundles' => 10,
+    'Products' => 10,
+    'Calls' => 410000,
+    'Emails' => 900000,
+    'EmailText' => 900000,
+    'Contacts' => 1150000,
+    'Leads' => 65000,
+    'Opportunities' => 80000,
+    'Cases' => 10000,
+    'Bugs' => 10,
+    'Meetings' => 31000,
+    'Tasks' => 430000,
+    'Notes' => 300000,
+    'Documents' => 2000,
+    'Categories' => 5,
+    'KBContents' => 10,
+    'SugarFavorites' => 18000,
+    'Reports' => 1000,
+);
+
+$sugarFavoritesModules = array(
+    'Accounts'      => 600,
+    'Contacts'      => 1800,
+    'Leads'         => 400,
+    'Opportunities' => 2000,
+    'Calls'         => 150,
+    'Meetings'      => 15,
+    'Cases'         => 50,
+    'Bugs'          => 1,
+    'Tasks'         => 350,
+    'Notes'         => 45,
+);
+
+$profile_opts = array(
+
+);
