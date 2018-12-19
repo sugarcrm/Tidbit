85b2a3b66530bbccd4b56d8b4fae7c14ed391f13
Alexey Klimko <aklimko@sugarcrm.com>
2018-12-19 14:37:18 +0100

    Add atlas profile.

---
+++ config/profiles/atlas.php
@@ -0,0 +1,33 @@
+<?php
+
+$modules = array(
+    'Tags' => 14000,
+    'EmailAddresses' => 9300000,
+    'ACLRoles' => 14,
+    'Users' => 55000,
+    'Teams' => 310,
+    'TeamSets' => 163000,
+    'Accounts' => 320000,
+    'Calls' => 1300000,
+    'Notifications' => 24000,
+    'Emails' => 3100000,
+    'EmailText' => 3650000,
+    'Contacts' => 8000000,
+    'Leads' => 3650000,
+    'Opportunities' => 600000,
+    'Meetings' => 60000,
+    'Tasks' => 2500000,
+    'Notes' => 9700000,
+    'Documents' => 250,
+    'Categories' => 2,
+    'Reports' => 3500,
+);
+
+$aliases = [
+    'ProductTemplatesFavorites' => 'ProdTPLFav',
+    'ProductsFavorites' => 'ProdFav',
+];
+
+$profile_opts = array(
+
+);
