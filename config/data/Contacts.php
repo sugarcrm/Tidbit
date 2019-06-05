290d8914824c41bc4b5cd1582c8668243803205a
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2019-06-05 12:03:16 +0300

    Sugar hash of specified value for contact portal passowrd

--- config/data/Contacts.php
+++ config/data/Contacts.php
@@ -56,7 +56,7 @@ $GLOBALS['dataTool']['Contacts']['primary_address_country'] = array('value' => "
 $GLOBALS['dataTool']['Contacts']['alt_address_country'] = array('value' => "'USA'");
 $GLOBALS['dataTool']['Contacts']['account_id'] = array('related' => array('module' => 'Accounts'));
 $GLOBALS['dataTool']['Contacts']['portal_name'] = array('incname' => 'contact');
-$GLOBALS['dataTool']['Contacts']['portal_password'] = array('same_sugar_hash' => 'portal_name');
+$GLOBALS['dataTool']['Contacts']['portal_password'] = array('sugar_hash' => "'asdf'");
 $GLOBALS['dataTool']['Contacts']['portal_active'] = array('value' => "'1'");
 $GLOBALS['dataTool']['Contacts']['phone_fax'] = array('phone' => true);
 $GLOBALS['dataTool']['Contacts']['phone_work'] = array('phone' => true);
